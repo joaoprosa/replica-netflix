8c9982d18be0bedcc1360ace1a3dea750f7ad458
Joao Paulo <vainawebcpx@gmail.com>
2019-11-06 12:30:29 -0300

    fazendo header

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+let lupa = document.querySelector('#lupa')
+let pesquina = document.querySelector('#pesquisa')
 
+lupa.addEventListener('click', function(){
+    if(pesquisa.style.display === 'flex'){
+        pesquisa.style.display = 'none'
+    }
+    else{
+        pesquisa.style.display = 'flex'
+    }
+})
+
+let seta = document.querySelector('.blockMenu__spanImageArrow')
+let usuario = document.querySelector('.blockMenu__spanPrincipal')
+
+seta.addEventListener('mouseenter' , function(){
+    if(usuario.style.display === 'flex'){
+        usuario.style.display = 'none'
+    }
+    else{
+        usuario.style.display = 'flex'
+    }
+})
